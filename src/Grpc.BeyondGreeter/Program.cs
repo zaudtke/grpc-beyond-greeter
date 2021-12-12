ab07f72816bf36eff8c29d71908a73a829dbce07
Al Zaudtke <zaudtke@gmail.com>
2021-12-12 14:38:56 -0500

    remove comments

--- src/Grpc.BeyondGreeter/Program.cs
+++ src/Grpc.BeyondGreeter/Program.cs
@@ -1,36 +1,3 @@
-//using System.Runtime.InteropServices;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.AspNetCore.Server.Kestrel.Core;
-//using Microsoft.Extensions.Hosting;
-
-//namespace Grpc.BeyondGreeter
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            CreateHostBuilder(args).Build().Run();
-//        }
-
-//        // Additional configuration is required to successfully run gRPC on macOS.
-//        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-//        public static IHostBuilder CreateHostBuilder(string[] args) =>
-//            Host.CreateDefaultBuilder(args)
-//                .ConfigureWebHostDefaults(webBuilder =>
-//                {
-//                    if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-//                    {
-//                        webBuilder.ConfigureKestrel(options =>
-//                        {
-//                            options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
-//                        });
-//                    }
-//                    webBuilder.UseStartup<Startup>();
-//                });
-//    }
-//}
-
-
 using Grpc.BeyondGreeter.Features.Greetings;
 using Grpc.BeyondGreeter.Features.Members;
 using Grpc.BeyondGreeter.Middleware;
